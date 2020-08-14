3c29dcac97d22046c1384813db110058fec6e10d
davidabutler92 <davidabutler92@yahoo.com>
2020-08-14 15:17:42 -0700

    add variable(total) for the sum of questions 1-5

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
 
     const firstName = $("#firstName").val();
-    const lastName = $("#lastName").val();
-    // console.log(firstName, lastName);
+    // console.log(firstName);
     const que1 = parseInt($("input#question-1").val());
     const que2 = parseInt($("input#question-2").val());
     const que3 = parseInt($("input#question-3").val());
     const que4 = parseInt($("input#question-4").val());
     const que5 = parseInt($("input#question-5").val());
-    console.log(que1, que2, que3, que4, que5);
+    // console.log(que1, que2, que3, que4, que5);
+    const total = que1 + que2 + que3 + que4 + que5
+    // console.log(total);
+    
   })
 });
