a180d3c3ce44f96efa3cee4b81d358791cc7ff08
davidabutler92 <davidabutler92@yahoo.com>
2020-08-14 15:05:56 -0700

    add remaining variables (que3, que4, que5) and test with console.log();

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ $(document).ready(function() {
     const lastName = $("#lastName").val();
     // console.log(firstName, lastName);
     const que1 = parseInt($("input#question-1").val());
-    const que2 = parseInt($("#question-2").val());
-    console.log(que2);
+    const que2 = parseInt($("input#question-2").val());
+    const que3 = parseInt($("input#question-3").val());
+    const que4 = parseInt($("input#question-4").val());
+    const que5 = parseInt($("input#question-5").val());
+    console.log(que1, que2, que3, que4, que5);
   })
 });
